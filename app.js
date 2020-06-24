323488d1eaeb8685c34cd69258f367114b3b93c4
root <k.ner@hotmail.com>
2020-06-24 15:44:25 +0000

    Update db url

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ const campgroundRoutes = require('./routes/campgrounds'),
 	  commentRoutes    = require('./routes/comments'),
 	  indexRoutes      = require('./routes/index');
 
-// mongoose.connect('mongodb://localhost:27017/yelp_camp_v12', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-mongoose.connect('mongodb+srv://dbUserYelpCamp:jegerkulogsmart@yelpcamp-nr0ry.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+/*mongoose.connect('mongodb+srv://dbUserYelpCamp:jegerkulogsmart@yelpcamp-nr0ry.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
 	console.log('Connected to DB!');
 }).catch(err => {
 	console.log('ERROR: ', err.message);
-});
+});*/
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 app.use(flash());
-// seedDB(); // Seed the database (having issues with campground.author.id.equals(currentUser._id)) 
+// seedDB(); // Seed the database (issues with campground.author.id.equals(currentUser._id)) 
 app.locals.moment = require('moment');
 
 // PASSPORT CONFIGURATION
