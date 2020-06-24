36404c86fe3372fc6579344af4951f6244f75519
root <k.ner@hotmail.com>
2020-06-24 13:39:07 +0000

    Add mongoDB Atlas

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const campgroundRoutes = require('./routes/campgrounds'),
 	  commentRoutes    = require('./routes/comments'),
 	  indexRoutes      = require('./routes/index');
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp_v12', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+// mongoose.connect('mongodb://localhost:27017/yelp_camp_v12', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect('mongodb+srv://dbUserYelpCamp:jegerkulogsmart@yelpcamp-nr0ry.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR: ', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
